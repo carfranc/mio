8612679e7c6d02bb1cff69f486f086e0066b6a93
luis-deyvit <deyvitayala@gamil.com>
2021-05-21 12:59:34 -0500

    agrege cambios

--- tarea.sql
+++ tarea.sql
@@ -8,3 +8,20 @@ create table documento
 )
 insert into documento values(12, 'repositorio', 'urgente')
 select * from documento
+
+create table listas
+(
+	id_lis int not null,
+	nombre varchar(20),
+	licencia varchar(10)
+)
+insert into listas values(12, 'repositorio', 'urgente')
+select * from lista
+create table listas3
+(
+	id_lis int not null,
+	nombre varchar(20),
+	licencia varchar(10)
+)
+insert into listas3 values(12, 'repositorio', 'urgente')
+select * from lista3
